4349e814e7c273164c7bb3280b4a1e18e1544445
Ilya Zhukov <ilya.beetle@gmail.com>
2018-01-03 22:29:22 +0300

    [CHS-3] - created IFasade, upd GameResultType.

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/GameResultType.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/GameResultType.java
@@ -1,5 +1,5 @@
 package mitrofanov82.edu.javagroup1.angry_chess.shared_model;
 
 public enum GameResultType {
-	WHITE_WIN, BLACK_WIN, DRAW, NOT_FINISHED
+	WHITE_WIN, BLACK_WIN, DRAW, NOT_FINISHED, END_GAME
 }
