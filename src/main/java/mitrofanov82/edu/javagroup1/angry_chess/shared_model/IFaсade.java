d7c7dee4827ea7b54a27ed3ad4c0b8e31aecad2d
IlyaBeetle <ilya.beetle@gmail.com>
2018-01-13 21:07:23 +0300

    hot fix

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/IFaсade.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/IFaсade.java
@@ -2,11 +2,13 @@ package mitrofanov82.edu.javagroup1.angry_chess.shared_model;
 
 import java.util.List;
 
+import mitrofanov82.edu.javagroup1.angry_chess.shared_model.exceptions.IncorrectMoveException;
+
 public interface IFaсade {
 
 	IGame createNewGame(long gameId, IPlayer p1, IPlayer p2);
 
-	IGame makeMove(IGame game, Coord from, Coord to);
+	IGame makeMove(IGame game, Coord from, Coord to) throws IncorrectMoveException;
 
 	IGame getGameById(long gameId);
 
