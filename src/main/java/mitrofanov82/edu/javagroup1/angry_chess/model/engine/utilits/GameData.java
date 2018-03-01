14bec66a00982ba63fa04b84485dfd85dd97b471
IlyaBeetle <ilya.beetle@gmail.com>
2018-03-02 01:52:24 +0300

    [CHS-6]-Fix the method addRecordToHistoryLog(). Updated the tabulation.

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/utilits/GameData.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/utilits/GameData.java
@@ -1,15 +1,12 @@
 package mitrofanov82.edu.javagroup1.angry_chess.model.engine.utilits;
 
 import java.util.ArrayList;
-
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.FigureType;
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.GameStatusType;
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.IGame;
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.IPlayer;
 
-/*
- * Class done and ready to use.
- */
+//Class done and ready to use.
 
 /**
  * Class for Data Transfer Object a <b>Game</b>, implement the interface
@@ -19,89 +16,80 @@ import mitrofanov82.edu.javagroup1.angry_chess.shared_model.IPlayer;
  * @author Ilya Zhukov
  */
 public class GameData implements IGame {
-	// Fields
-	private IPlayer whitePlayer;
-	private IPlayer blackPlayer;
-	private IPlayer playerToMove;
-	private ArrayList<String> historyLog;
-	private FigureType[][] currentPosition;
-	private GameStatusType gameStatus;
-	private long id;
+    // Fields
+    private IPlayer whitePlayer;
+    private IPlayer blackPlayer;
+    private IPlayer playerToMove;
+    private ArrayList<String> historyLog;
+    private FigureType[][] currentPosition;
+    private GameStatusType gameStatus;
+    private long id;
 
-	// Constructors
-	/**
-	 * Constructor of the DTO <b>Game</b>.
-	 * 
-	 * @param id
-	 *            - unique game identification number.
-	 * @param whitePlayer
-	 *            - Data of player, who play of white figures.
-	 * @param blackPlayer
-	 *            - Data of player, who play of black figures.
-	 */
-	public GameData(long id, IPlayer whitePlayer, IPlayer blackPlayer) {
-		this.id = id;
-		this.whitePlayer = whitePlayer;
-		this.blackPlayer = blackPlayer;
-		this.playerToMove = whitePlayer;
-		this.gameStatus = GameStatusType.IN_PROGRESS;
-		this.currentPosition = ManagerFiguresUtility.getStartPositions();
-		historyLog = new ArrayList<>();
-	}
+    // Constructors
+    /**
+     * Constructor of the DTO <b>Game</b>.
+     * 
+     * @param id
+     *            - unique game identification number.
+     * @param whitePlayer
+     *            - Data of player, who play of white figures.
+     * @param blackPlayer
+     *            - Data of player, who play of black figures.
+     */
+    public GameData(long id, IPlayer whitePlayer, IPlayer blackPlayer) {
+        this.id = id;
+        this.whitePlayer = whitePlayer;
+        this.blackPlayer = blackPlayer;
+        this.playerToMove = whitePlayer;
+        this.gameStatus = GameStatusType.IN_PROGRESS;
+        this.currentPosition = ManagerFiguresUtility.getStartPositions();
+        historyLog = new ArrayList<>();
+    }
 
-	// Setters
-	public void setGameStatus(GameStatusType gameStatus) {
-		this.gameStatus = gameStatus;
-	}
+    // Setters
+    public void setGameStatus(GameStatusType gameStatus) {
+        this.gameStatus = gameStatus;
+    }
 
-	public void setPlayerToMove(IPlayer playerToMove) {
-		this.playerToMove = playerToMove;
-	}
+    public void setPlayerToMove(IPlayer playerToMove) {
+        this.playerToMove = playerToMove;
+    }
 
-	public void setCurrentPosition(FigureType[][] currentPosition) {
-		this.currentPosition = currentPosition;
-	}
+    public void setCurrentPosition(FigureType[][] currentPosition) {
+        this.currentPosition = currentPosition;
+    }
 
-	/**
-	 * Каждое новое добавляемое значение будет первым в списке с идексом 0.
-	 * 
-	 * @param lastMove
-	 *            - строковое обозначение последнего хода в виде (e2 – e4).
-	 */
-	public void addRecordToHistoryLog(String lastMove) {
-		ArrayList<String> tempArrayStrings = new ArrayList<>();
-		tempArrayStrings.add(lastMove);
-		tempArrayStrings.addAll(historyLog);
-		this.historyLog = tempArrayStrings;
-	}
+    public void addRecordToHistoryLog(String lastMove) {
+        historyLog.add(0, lastMove);
+    }
 
-	// Getters
-	public IPlayer getWhitePlayer() {
-		return whitePlayer;
-	}
+    // Getters
+    public IPlayer getWhitePlayer() {
+        return whitePlayer;
+    }
 
-	public IPlayer getBlackPlayer() {
-		return blackPlayer;
-	}
+    public IPlayer getBlackPlayer() {
+        return blackPlayer;
+    }
 
-	public IPlayer getPlayerToMove() {
-		return playerToMove;
-	}
+    public IPlayer getPlayerToMove() {
+        return playerToMove;
+    }
 
-	public ArrayList<String> getHistoryLog() {
-		return historyLog;
-	}
+    public ArrayList<String> getHistoryLog() {
+        return historyLog;
+    }
 
-	public FigureType[][] getCurrentPosition() {
-		return currentPosition;
-	}
+    public FigureType[][] getCurrentPosition() {
+        return currentPosition;
+    }
 
-	public long getGameId() {
-		return id;
-	}
+    public long getGameId() {
+        return id;
+    }
 
-	public GameStatusType getGameStatus() {
-		return gameStatus;
-	}
+    public GameStatusType getGameStatus() {
+        return gameStatus;
+    }
 
 }
