d785843d2ee702cb7b7545725fafa8d25e0f5836
IlyaBeetle <ilya.beetle@gmail.com>
2018-03-02 01:55:42 +0300

    [CHS-6]-Little a fixed. Updated the tabulation.

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/EngineTheFirst.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/EngineTheFirst.java
@@ -16,112 +16,114 @@ import mitrofanov82.edu.javagroup1.angry_chess.shared_model.IPlayer;
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.exceptions.ChessModelException;
 import mitrofanov82.edu.javagroup1.angry_chess.shared_model.exceptions.IncorrectMoveException;
 
-/*
- * Class done and ready to use.
- */
+//Class done and ready to use.
 
 /**
  * Class implement model Angry Chess <br>
  * Name engine: "The First" <br>
- * Version engine: 0.1 <br>
+ * Version engine: 0.1.1 <br>
  * 
- * @author Ilya Zhukov <br>
+ * @author Ilya Zhukov
  */
 public class EngineTheFirst {
 
-	HashMap<Long, GameData> launchedGames;
+    private HashMap<Long, GameData> launchedGames;
 
-	public EngineTheFirst() {
-		this.launchedGames = new HashMap<>();
-	}
+    public EngineTheFirst() {
+        this.launchedGames = new HashMap<>();
+    }
 
-	public IGame createNewGame(long gameId, IPlayer whitePlayer, IPlayer blackPlayer) throws ChessModelException {
-		// Check if there is such a game
-		if (launchedGames.containsKey(gameId)) {
-			throw new ChessModelException("This ID( " + gameId + " ) is already taken");
-		}
+    public IGame createNewGame(long gameId, IPlayer whitePlayer, IPlayer blackPlayer) throws ChessModelException {
+        // Check if there is such a game
+        if (launchedGames.containsKey(gameId)) {
+            throw new ChessModelException("This ID( " + gameId + " ) is already taken");
+        }
 
-		IGame newGame = new GameData(gameId, whitePlayer, blackPlayer);
-		launchedGames.put(gameId, (GameData) newGame);
-		return newGame;
-	}
+        GameData newGame = new GameData(gameId, whitePlayer, blackPlayer);
+        launchedGames.put(gameId, newGame);
+        return newGame;
+    }
 
-	public IGame makeMove(IGame game, Coord from, Coord to) throws IncorrectMoveException {
-		// Check if there is such a game
-		if (!launchedGames.containsKey(game.getGameId())) {
-			throw new ChessModelRuntimeException("This game (id:" + game.getGameId() + ") does not exist");
-		}
+    public IGame makeMove(IGame game, Coord from, Coord to) throws IncorrectMoveException {
+        // Check if there is such a game
+        if (!launchedGames.containsKey(game.getGameId())) {
+            throw new ChessModelRuntimeException("This game (id:" + game.getGameId() + ") does not exist");
+        }
 
-		GameData currentGame = launchedGames.get(game.getGameId());
-		if(RulesModeratorUtility.checkMove(currentGame, from, to)) {
-			currentGame = ManagerFiguresUtility.makeMove(currentGame, from, to);
-			
-			return currentGame;
-		}else {
-			throw new IncorrectMoveException("Incorrectly move");
-		}
-	
-	}
+        GameData currentGame = launchedGames.get(game.getGameId());
 
-	public IGame endGame(IGame game, GameStatusType status) {
-		// Check if there is such a game
-		if (!launchedGames.containsKey(game.getGameId())) {
-			throw new ChessModelRuntimeException("This game (id:" + game.getGameId() + ") does not exist");
-		}
+        if (RulesModeratorUtility.checkMove(currentGame, from, to)) {
+            currentGame = ManagerFiguresUtility.makeMove(currentGame, from, to);
+            return currentGame;
+        } else {
+            throw new IncorrectMoveException("Incorrectly move");
+        }
+    }
 
-		GameData currentGame = (GameData) game;
-		currentGame.setGameStatus(status);
+    public IGame endGame(IGame game, GameStatusType status) {
+        // Check if there is such a game
+        if (!launchedGames.containsKey(game.getGameId())) {
+            throw new ChessModelRuntimeException("This game (id:" + game.getGameId() + ") does not exist");
+        }
 
-		if (status == GameStatusType.BLACK_WIN) {
-			currentGame.addRecordToHistoryLog("Status of game to changing on \"Black defeated\"");
-			launchedGames.remove(currentGame.getGameId());
-		}
-		if (status == GameStatusType.WHITE_WIN) {
-			currentGame.addRecordToHistoryLog("Status of game to changing on \"White defeated\"");
-			launchedGames.remove(currentGame.getGameId());
-		}
-		if (status == GameStatusType.END_GAME_NO_RESULT) {
-			currentGame.addRecordToHistoryLog("Status of game to changing on \"End of the game with no result\"");
-			launchedGames.remove(currentGame.getGameId());
-		}
-		if (status == GameStatusType.DRAW) {
-			currentGame.addRecordToHistoryLog("Status of game to changing on \"Draw\"");
-			launchedGames.remove(currentGame.getGameId());
-		}
+        GameData currentGame = launchedGames.get(game.getGameId());
 
-		return currentGame;
-	}
+        currentGame.setGameStatus(status);
+        currentGame.addRecordToHistoryLog(messageSetStatus(status));
+        launchedGames.remove(currentGame.getGameId());
+        return currentGame;
+    }
 
-	public GameStatusType getGameStatus(long gameId) {
-		if (launchedGames.containsKey(gameId)) {
-			return launchedGames.get(gameId).getGameStatus();
+    public GameStatusType getGameStatus(long gameId) {
+        if (launchedGames.containsKey(gameId)) {
+            return launchedGames.get(gameId).getGameStatus();
+        } else {
+            throw new ChessModelRuntimeException("This game (id:" + gameId + ") does not exist");
+        }
+    }
 
-		} else {
-			throw new ChessModelRuntimeException("This game (id:" + gameId + ") does not exist");
-		}
-	}
+    public IGame getGameById(long gameId) {
+        if (launchedGames.containsKey(gameId)) {
+            return launchedGames.get(gameId);
+        } else {
+            throw new ChessModelRuntimeException("This game (id:" + gameId + ") does not exist");
+        }
+    }
 
-	public IGame getGameById(long gameId) {
-		if (launchedGames.containsKey(gameId)) {
-			return launchedGames.get(gameId);
+    public List<IGame> getCurrentGames() {
+        if (!launchedGames.isEmpty()) {
+            List<IGame> listGames = new ArrayList<>();
+            Set<Long> keySet = launchedGames.keySet();
+            for (Long key : keySet) {
+                listGames.add(launchedGames.get(key));
+            }
+            return listGames;
+        } else {
+            throw new ChessModelRuntimeException("The model does not contain running games");
+        }
+    }
 
-		} else {
-			throw new ChessModelRuntimeException("This game (id:" + gameId + ") does not exist");
-		}
-	}
+    private String messageSetStatus(GameStatusType status) {
+        if (status == GameStatusType.BLACK_WIN) {
+            return "Status of game to changing on \"Black defeated\"";
+        }
+        if (status == GameStatusType.WHITE_WIN) {
+            return "Status of game to changing on \"White defeated\"";
+        }
+        if (status == GameStatusType.END_GAME_NO_RESULT) {
+            return "Status of game to changing on \"End of the game with no result\"";
+        }
+        if (status == GameStatusType.DRAW) {
+            return "Status of game to changing on \"Draw\"";
+        }
 
-	public List<IGame> getCurrentGames() {
-		if (!launchedGames.isEmpty()) {
-			List<IGame> listGames = new ArrayList<>();
-			Set<Long> keySet = launchedGames.keySet();
-			for (Long key : keySet) {
-				listGames.add(launchedGames.get(key));
-			}
-			return listGames;
+        if (status == GameStatusType.END_GAME_NO_RESULT) {
+            return "Status of game to changing on \"End game no result\"";
+        }
 
-		} else {
-			throw new ChessModelRuntimeException("The model does not contain running games");
-		}
-	}
+        else {
+            throw new ChessModelRuntimeException("Invalid request to status for the to end game");
+        }
+    }
 
 }
--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/utilits/RulesModeratorUtility.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/model/engine/utilits/RulesModeratorUtility.java
@@ -7,89 +7,95 @@ import mitrofanov82.edu.javagroup1.angry_chess.shared_model.FigureType;
  * Moderator Rules - checks the moves for compliance with the rules and logic of
  * the game. <br>
  * 
- * @author Ilya Zhukov
- *
+ * @author Ilya Zhukov, Maksim Kosmachev
  */
 public class RulesModeratorUtility {
 
-	public static boolean checkMove(GameData currentGame, Coord from, Coord to) {
-		//FIXME for tests
-		/*if (!checkAdequacy(currentGame, from, to)) {
-			return false;
-		}
+    /**
+     * //TODO add doc-comment RulesModeratorUtility.checkMove();
+     * 
+     * @param currentGame
+     * @param from
+     * @param to
+     * @return
+     */
+    public static boolean checkMove(GameData currentGame, Coord from, Coord to) {
+        // FIXME for tests
+        /*
+         * if (!checkAdequacy(currentGame, from, to)) { return false; }
+         * 
+         * if (!checkPiece(currentGame, from, to)) { return false; }
+         */
 
-		if (!checkPiece(currentGame, from, to)) {
-			return false;
-		}*/
-		
-		return true;
-	}
+        return true;
+    }
 
-	private static boolean checkAdequacy(GameData currentGame, Coord from, Coord to) {
-		FigureType[][] board = currentGame.getCurrentPosition();
-		FigureType stepFrom = board[from.getRow()][from.getCol()];
-		FigureType stepTo=board[to.getRow()][to.getCol()];
-		//if(stepTo=null || stepFrom.)
-		
-		// TODO Create method checkLogic() in the class RulesModeratorUtility
-		// from piece = null
-		// "from" and "to" a pieces is one color
-		//
-		return false;
-	}
+    private static boolean checkAdequacy(GameData currentGame, Coord from, Coord to) {
+        FigureType[][] board = currentGame.getCurrentPosition();
+        FigureType stepFrom = board[from.getRow()][from.getCol()];
+        FigureType stepTo = board[to.getRow()][to.getCol()];
+        // if(stepTo=null || stepFrom.)
 
-	private static boolean checkPiece(GameData currentGame, Coord from, Coord to) {
-		FigureType[][] blackboard = currentGame.getCurrentPosition();
-		FigureType curentPiece = blackboard[from.getRow()][from.getCol()];
+        // TODO Create method checkLogic() in the class RulesModeratorUtility
+        // from piece != null
+        // "from" and "to" a pieces is not one color
+        // "from" is figure current Player
+        // Coordinates from = 0...7 and to = 0...7
+        return false;
+    }
 
-		if (curentPiece == FigureType.BLACK_PAWN || curentPiece == FigureType.WHITE_PAWN) {
-			return checkMovePawn();
-		}
-		if (curentPiece == FigureType.BLACK_ROOK || curentPiece == FigureType.WHITE_ROOK) {
-			return checkMoveRook();
-		}
-		if (curentPiece == FigureType.BLACK_KNIGHT || curentPiece == FigureType.WHITE_KNIGHT) {
-			return checkMoveKnight();
-		}
-		if (curentPiece == FigureType.BLACK_BISHOP || curentPiece == FigureType.WHITE_BISHOP) {
-			return checkMoveBishop();
-		}
-		if (curentPiece == FigureType.BLACK_QUEEN || curentPiece == FigureType.WHITE_QUEEN ) {
-			return checkMoveQueen();
-		}
-		if (curentPiece == FigureType.BLACK_KING || curentPiece == FigureType.WHITE_KING) {
-			return checkMoveKing();
-		}
-		return false;
-	}
+    private static boolean checkPiece(GameData currentGame, Coord from, Coord to) {
+        FigureType[][] blackboard = currentGame.getCurrentPosition();
+        FigureType curentPiece = blackboard[from.getRow()][from.getCol()];
 
-	private static boolean checkMovePawn() {
-		// TODO Create method checkMovePawn() in the class RulesModeratorUtility
-		return false;
-	}
+        if (curentPiece == FigureType.BLACK_PAWN || curentPiece == FigureType.WHITE_PAWN) {
+            return checkMovePawn();
+        }
+        if (curentPiece == FigureType.BLACK_ROOK || curentPiece == FigureType.WHITE_ROOK) {
+            return checkMoveRook();
+        }
+        if (curentPiece == FigureType.BLACK_KNIGHT || curentPiece == FigureType.WHITE_KNIGHT) {
+            return checkMoveKnight();
+        }
+        if (curentPiece == FigureType.BLACK_BISHOP || curentPiece == FigureType.WHITE_BISHOP) {
+            return checkMoveBishop();
+        }
+        if (curentPiece == FigureType.BLACK_QUEEN || curentPiece == FigureType.WHITE_QUEEN) {
+            return checkMoveQueen();
+        }
+        if (curentPiece == FigureType.BLACK_KING || curentPiece == FigureType.WHITE_KING) {
+            return checkMoveKing();
+        }
+        return false;
+    }
 
-	private static boolean checkMoveRook() {
-		// TODO Create method checkMoveRook() in the class RulesModeratorUtility
-		return false;
-	}
+    private static boolean checkMovePawn() {
+        // TODO Create method checkMovePawn() in the class RulesModeratorUtility
+        return false;
+    }
 
-	private static boolean checkMoveKnight() {
-		// TODO Create method checkMoveKnight() in the class RulesModeratorUtility
-		return false;
-	}
+    private static boolean checkMoveRook() {
+        // TODO Create method checkMoveRook() in the class RulesModeratorUtility
+        return false;
+    }
 
-	private static boolean checkMoveBishop() {
-		// TODO Create method checkMoveBishop() in the class RulesModeratorUtility
-		return false;
-	}
+    private static boolean checkMoveKnight() {
+        // TODO Create method checkMoveKnight() in the class RulesModeratorUtility
+        return false;
+    }
 
-	private static boolean checkMoveQueen() {
-		// TODO Create method checkMoveQueen() in the class RulesModeratorUtility
-		return false;
-	}
+    private static boolean checkMoveBishop() {
+        // TODO Create method checkMoveBishop() in the class RulesModeratorUtility
+        return false;
+    }
 
-	private static boolean checkMoveKing() {
-		// TODO Create method checkMoveKing() in the class RulesModeratorUtility
-		return false;
-	}
+    private static boolean checkMoveQueen() {
+        // TODO Create method checkMoveQueen() in the class RulesModeratorUtility
+        return false;
+    }
+
+    private static boolean checkMoveKing() {
+        // TODO Create method checkMoveKing() in the class RulesModeratorUtility
+        return false;
+    }
 }
