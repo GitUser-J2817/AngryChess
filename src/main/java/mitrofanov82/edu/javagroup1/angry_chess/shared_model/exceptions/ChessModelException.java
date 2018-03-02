71b49aec1a180720191f7f78d19be2be2808a08f
Ilya Zhukov <ilya.beetle@gmail.com>
2018-03-02 10:39:21 +0300

    Revert last error merge

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/exceptions/ChessModelException.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/exceptions/ChessModelException.java
@@ -1,6 +1,5 @@
 package mitrofanov82.edu.javagroup1.angry_chess.shared_model.exceptions;
 
-@SuppressWarnings("serial")
 public class ChessModelException extends Exception {
 
     public ChessModelException(String message) {
