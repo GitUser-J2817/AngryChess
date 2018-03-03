864cc2e16a9dfb2b01010c74dda41822ae99ecfd
IlyaBeetle <ilya.beetle@gmail.com>
2018-03-03 19:18:08 +0300

    Remove SuppressWarnings

--- src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/exceptions/IncorrectMoveException.java
+++ src/main/java/mitrofanov82/edu/javagroup1/angry_chess/shared_model/exceptions/IncorrectMoveException.java
@@ -1,6 +1,5 @@
 package mitrofanov82.edu.javagroup1.angry_chess.shared_model.exceptions;
 
-@SuppressWarnings("serial")
 public class IncorrectMoveException extends ChessModelException {
 
     public IncorrectMoveException(String message) {
